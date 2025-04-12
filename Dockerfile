a06a4205f2e4024d6150d5178b6e9724710f82b8
Амаль <om1ji.music@gmail.com>
2025-04-12 16:11:36 +0300

    Use PostgreSQL instead of SQLite db

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Копирование кода приложения
 COPY . .
 
-# Запуск миграций и сервера
 CMD ["sh", "-c", "cd wb_wms && python manage.py migrate && python manage.py runserver 0.0.0.0:8000"] 
--- wb_wms/wb_wms/settings.py
+++ wb_wms/wb_wms/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = "wb_wms.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "wb_wms"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
